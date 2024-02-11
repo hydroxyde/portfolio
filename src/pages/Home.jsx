f156aba467dfa4de1f709db5a11766a9555f88dd
Sami Hammami <samih@lablup.com>
2024-02-12 00:17:48 +0900

    change lighting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,11 +28,11 @@ const Home = () => {
       <Canvas className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          {/* <pointLight /> */}
+          {/* <spotLight /> */}
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
           <Island
             position={islandPosition}
